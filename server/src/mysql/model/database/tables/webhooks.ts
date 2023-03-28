598ed3c0cc652d938d22e0df4f059457cc8dfda1
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-03-28 15:23:52 -0600

    Primary key for webhook ids

--- server/src/mysql/model/database/tables/webhooks.ts
+++ server/src/mysql/model/database/tables/webhooks.ts
@@ -1,21 +1,25 @@
 import { Table } from "@upvotr/mysql-query-builder";
 import { getTableName, tables } from "../tables";
 
-export const webhooks = new Table(getTableName(tables.webhooks), [
-  {
-    name: "trigger",
-    type: "TINYINT UNSIGNED",
-    notNull: true
-  },
-  {
-    name: "url",
-    type: "TEXT",
-    notNull: true
-  },
-  {
-    name: "webhookId",
-    type: "SMALLINT UNSIGNED",
-    notNull: true,
-    raw: "`webhookId` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT"
-  }
-] as const);
+export const webhooks = new Table(
+  getTableName(tables.webhooks),
+  [
+    {
+      name: "trigger",
+      type: "TINYINT UNSIGNED",
+      notNull: true
+    },
+    {
+      name: "url",
+      type: "TEXT",
+      notNull: true
+    },
+    {
+      name: "webhookId",
+      type: "SMALLINT UNSIGNED",
+      notNull: true,
+      raw: "`webhookId` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT"
+    }
+  ] as const,
+  "PRIMARY KEY (`webhookId`)"
+);
