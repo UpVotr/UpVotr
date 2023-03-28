094c8d4ac77d05c1c3c196727e19e794a84a2b4a
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-03-28 11:10:07 -0600

    That wouldn't have worked...

--- server/src/mysql/model/database/database.ts
+++ server/src/mysql/model/database/database.ts
@@ -7,7 +7,7 @@ import { sessions } from "./tables/sessions";
 import { tagDefinitions, tags } from "./tables/tags";
 import { users } from "./tables/users";
 
-export const database = new Database(config.mysql.database.name, [
+const tables = [
   comments,
   posts,
   roles,
@@ -16,20 +16,11 @@ export const database = new Database(config.mysql.database.name, [
   tags,
   tagDefinitions,
   users
-]);
+];
+
+export const database = new Database(config.mysql.database.name, tables);
 
 export const configureDatabase = `${database.create(true)};
 ${database.use()};
-${database
-  .ensureTables([
-    "comments",
-    "posts",
-    "roles",
-    "roleDefinitions",
-    "sessions",
-    "tags",
-    "tagDefinitions",
-    "users"
-  ])
-  .join(";\n")};
+${database.ensureTables(tables.map((table) => table.rawName())).join(";\n")};
 ${commentAutoIncrementTrigger};`;
