dc31e426655ff3564722b7bdcb370d36cb5f7d1c
ErrorGamer2000 <errorgamer2000@outlook.com>
2023-03-28 15:11:54 -0600

    Version management for migration

---
+++ server/src/mysql/model/database/tables/version.ts
@@ -0,0 +1,41 @@
+import { Table } from "@upvotr/mysql-query-builder";
+import { getTableName, tables } from "../tables";
+
+const upvotrVersion = {
+  major: 0,
+  minor: 1,
+  bugFix: 0
+};
+
+export const version = new Table(
+  getTableName(tables.version),
+  [
+    {
+      name: "key",
+      type: "BOOLEAN",
+      notNull: true,
+      default: "FALSE"
+    },
+    {
+      name: "major",
+      type: "TINYINT",
+      notNull: true,
+      default: "0"
+    },
+    {
+      name: "minor",
+      type: "TINYINT",
+      notNull: true,
+      default: "0"
+    },
+    {
+      name: "bugFix",
+      type: "TINYINT",
+      notNull: true,
+      default: "0"
+    }
+  ] as const,
+  "PRIMARY KEY (`key`)"
+);
+
+export const initVersion = `INSERT IGNORE INTO ${version.aliasedName()}() VALUES()`;
